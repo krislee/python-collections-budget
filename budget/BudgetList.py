6a04edff7e0ef6d282fb08ddca7d4faba74bc69e
Kristy Lee <kristy.lee@fool.com>
2021-11-04 17:24:04 -0400

    Making BudgetList as an iterable

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from . import Expense
 
 class BudgetList:
@@ -20,6 +21,26 @@ class BudgetList:
     def __len__(self):
         return len(self.expenses) + len(self.overages)
 
+    ##############
+    ## Module 3 ##
+    ##############
+
+    # Goal is to create our own iterator for Budget class
+    # To create iterator, follow the iterator protocol, that is have __iter__ and __next__
+    def __iter__(self):
+        # Since self.expenses and self.overages are lists, they each have built-in
+        # iterators, so we will use their built-in iterators
+        self.iter_e = iter(self.expenses) # returns an iterator
+        self.iter_o = iter(self.overages) # returns an iterator
+        return self
+
+    def __next__(self):
+        try:
+            return self.iter_e.__next__()
+        except StopIteration as stop:
+            return self.iter_o.__next__()
+
+
 def main():
     myBudgetList = BudgetList(1200)
     expenses = Expense.Expenses()
@@ -28,6 +49,21 @@ def main():
     for expense in expenses.list:
         myBudgetList.append(expense.amount)
     print("The count of all expenses: " + str(len(myBudgetList)))
+
+    for entry in myBudgetList:
+        print(entry)
+
+    fig, ax = plt.subplots()
+    labels = ['Expenses', 'Overages', 'Budget']
+    values = [myBudgetList.sum_expenses, myBudgetList.sum_overages, myBudgetList.budget]
+
+    ax.bar(labels, values, color=['green', 'red', 'blue'])
+    ax.set_title('Your total expenses vs. total budget')
+
+    plt.show()
+
+
+
 
 if __name__ == "__main__":
     main()
